7836577c28f808aec2bf0d1a2d363e4589ad20ef
HoNgocAn <anhongoc1306@gmail.com>
2023-08-04 20:55:06 +0700

    nop bai

--- project/src/ss3_Array/Bai_tap/DemSoLanXuatHienCuaKyTu.java
+++ project/src/ss3_Array/Bai_tap/DemSoLanXuatHienCuaKyTu.java
@@ -1,5 +1,25 @@
 package ss3_Array.Bai_tap;
 
-public class DemSoLanXuatHienCuaKyTu {
+import java.util.Scanner;
 
+public class DemSoLanXuatHienCuaKyTu {
+    public static void main(String[] args) {
+
+        String str = "abcddd";
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhập ký tự");
+        String name = input.nextLine();
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str[i].equals(name)) {
+                count++;
+            }
+        }
+        if (count > 0) {
+            System.out.println("Ký tự " + name + "xuất hiện" + count + " lần trong chuỗi");
+        } else {
+            System.out.println("Ký tự " + name + " không xuất hiện trong chuỗi");
+        }
+
+    }
 }
