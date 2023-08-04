0e12062ceae2b453052b92521155b7daac194d3a
HoNgocAn <anhongoc1306@gmail.com>
2023-08-04 21:02:21 +0700

    nop bai

--- project/src/ss3_Array/Bai_tap/DemSoLanXuatHienCuaKyTu.java
+++ project/src/ss3_Array/Bai_tap/DemSoLanXuatHienCuaKyTu.java
@@ -4,22 +4,22 @@ import java.util.Scanner;
 
 public class DemSoLanXuatHienCuaKyTu {
     public static void main(String[] args) {
-
         String str = "abcddd";
         Scanner input = new Scanner(System.in);
         System.out.println("Nhập ký tự");
-        String name = input.nextLine();
+        char name = input.next().charAt(0);
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
-            if (str[i].equals(name)) {
+            if (name == str.charAt(i)) {
                 count++;
             }
         }
-        if (count > 0) {
-            System.out.println("Ký tự " + name + "xuất hiện" + count + " lần trong chuỗi");
-        } else {
-            System.out.println("Ký tự " + name + " không xuất hiện trong chuỗi");
+            if (count > 0) {
+                 System.out.println("Ký tự " + name + " xuất hiện " + count + " lần trong chuỗi");
+            } else {
+                 System.out.println("Ký tự " + name + " không xuất hiện trong chuỗi");
+                }
+            }
         }
 
-    }
-}
+
