6379c2eb525460b071c1251096063e67226a179f
HoNgocAn <anhongoc1306@gmail.com>
2023-08-08 07:31:20 +0700

    nop bai

--- project/src/ss4_Class_Object/Bai_tap/Calculate2.java
+++ project/src/ss4_Class_Object/Bai_tap/Calculate2.java
@@ -1,17 +1,7 @@
 package ss4_Class_Object.Bai_tap;
 
 public class Calculate2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) {}};
 
-        StopWatch watch = new StopWatch();
-        watch.start();
-
-        for (int i = 0; i <= 100000; i++) {
-            System.out.println(i);
-        }
-        watch.stop();
-        System.out.println(watch.getElapsedtime());
-    }
-}
 
 
--- project/src/ss4_Class_Object/Bai_tap/StopWatch.java
+++ project/src/ss4_Class_Object/Bai_tap/StopWatch.java
@@ -17,9 +17,11 @@ public class StopWatch {
         return this.endTime;
     }
     public void start(){
+
         this.startTime = System.currentTimeMillis();
     }
     public void stop(){
+
         this.endTime = System.currentTimeMillis();
     }
     public long getElapsedTime(){
