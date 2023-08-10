606965c598c867c4d19b379da6074cbd75127d56
HoNgocAn <anhongoc1306@gmail.com>
2023-08-10 21:46:01 +0700

    nop bai

---
+++ project/src/ss7_AbstractClass_interface/Bai_tap/Bai1/Square.java
@@ -0,0 +1,45 @@
+package ss7_AbstractClass_interface.Bai_tap.Bai1;
+
+import ss7_AbstractClass_interface.Bai_tap.Bai2.Colorable;
+import ss7_AbstractClass_interface.Bai_tap.Bai2.Shape;
+
+public class Square extends Shape implements Colorable {
+    private double width;
+
+    public Square() {
+    }
+
+    public Square(double width) {
+        this.width = width;
+    }
+
+    public Square(String color, boolean filled, double width) {
+        super(color, filled);
+        this.width = width;
+    }
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    @Override
+    public String toString() {
+        return "Square width=" + getWidth();
+    }
+
+    public double getPerimeter() {
+        return getWidth() * getWidth();
+    }
+
+    @Override
+    public String howToColor() {
+        if(super.isFilled()) {
+            return getColor();
+        }
+
+        return "";
+    }
+}
--- project/src/ss7_AbstractClass_interface/Bai_tap/Bai1/Test.java
+++ project/src/ss7_AbstractClass_interface/Bai_tap/Bai1/Test.java
@@ -2,19 +2,49 @@ package ss7_AbstractClass_interface.Bai_tap.Bai1;
 
 import ss6_Kethua.Thuc_hanh.Square;
 
-public class Test {
-    public static class Main {
-        public static void main(String[] args) {
+class ResizeableTest {
+    public static void main(String[] args) {
 
-            Resizeable[] shapes = {new Circle(5), new Rectangle(3, 4), (Resizeable) new Square(2)};
+        Circle circle1 = new Circle();
+        Circle circle2 = new Circle(20);
 
-            for (Resizeable shape : shapes) {
-                double oldArea = shape.getArea();
-                double resizePercentage = Math.random() * 100;
-                shape.resize(resizePercentage);
-                double newArea = shape.getArea();
-                System.out.println("Dien tich " + shape + " truoc va sau khi resize la " + resizePercentage + "%: " + oldArea + " va " + newArea);
-            }
-        }
+        System.out.println("Giá trị đầu hình tròn");
+        System.out.println(circle1);
+        System.out.println(circle2);
+
+        circle1.resize(Math.random() * 100);
+        circle2.resize(Math.random() * 100);
+        System.out.println("Giá trị sau sử dụng resize");
+        System.out.println(circle1);
+        System.out.println(circle2);
+        System.out.println("");
+
+        Rectangle rectangle1 = new Rectangle();
+        Rectangle rectangle2 = new Rectangle(20, 40);
+
+        System.out.println("Giá trị đầu hình chữ nhật");
+        System.out.println(rectangle1);
+        System.out.println(rectangle2);
+
+        rectangle1.resize(Math.random() * 100);
+        rectangle2.resize(Math.random() * 100);
+        System.out.println("Giá trị sau sử dụng resize");
+        System.out.println(rectangle1);
+        System.out.println(rectangle2);
+        System.out.println("");
+
+        Square square1 = new Square();
+        Square square2 = new Square(15);
+
+        System.out.println("Giá trị đầu hình chữ vuông");
+        System.out.println(square1);
+        System.out.println(square2);
+
+        square1.resize(Math.random() * 100);
+        square2.resize(Math.random() * 100);
+        System.out.println("Giá trị sau sử dụng resize");
+        System.out.println(square1);
+        System.out.println(square2);
+        System.out.println("");
     }
 }
