fff81c3a397bab9423cb2cce30dd02e5300bf18a
HoNgocAn <anhongoc1306@gmail.com>
2023-08-10 21:49:50 +0700

    nop bai

--- project/src/ss7_AbstractClass_interface/Bai_tap/Bai1/Test.java
+++ project/src/ss7_AbstractClass_interface/Bai_tap/Bai1/Test.java
@@ -8,13 +8,13 @@ class ResizeableTest {
         Circle circle1 = new Circle();
         Circle circle2 = new Circle(20);
 
-        System.out.println("Giá trị đầu hình tròn");
+        System.out.println("Before");
         System.out.println(circle1);
         System.out.println(circle2);
 
         circle1.resize(Math.random() * 100);
         circle2.resize(Math.random() * 100);
-        System.out.println("Giá trị sau sử dụng resize");
+        System.out.println("After");
         System.out.println(circle1);
         System.out.println(circle2);
         System.out.println("");
@@ -22,13 +22,13 @@ class ResizeableTest {
         Rectangle rectangle1 = new Rectangle();
         Rectangle rectangle2 = new Rectangle(20, 40);
 
-        System.out.println("Giá trị đầu hình chữ nhật");
+        System.out.println("Before");
         System.out.println(rectangle1);
         System.out.println(rectangle2);
 
         rectangle1.resize(Math.random() * 100);
         rectangle2.resize(Math.random() * 100);
-        System.out.println("Giá trị sau sử dụng resize");
+        System.out.println("After");
         System.out.println(rectangle1);
         System.out.println(rectangle2);
         System.out.println("");
@@ -36,13 +36,13 @@ class ResizeableTest {
         Square square1 = new Square();
         Square square2 = new Square(15);
 
-        System.out.println("Giá trị đầu hình chữ vuông");
+        System.out.println("Before");
         System.out.println(square1);
         System.out.println(square2);
 
         square1.resize(Math.random() * 100);
         square2.resize(Math.random() * 100);
-        System.out.println("Giá trị sau sử dụng resize");
+        System.out.println("After");
         System.out.println(square1);
         System.out.println(square2);
         System.out.println("");
