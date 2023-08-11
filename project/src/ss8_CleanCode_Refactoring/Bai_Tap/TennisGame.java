1bf2f74e5105de249a332b45d83260eb082d3070
HoNgocAn <anhongoc1306@gmail.com>
2023-08-11 19:01:06 +0700

    nop bai

--- project/src/ss8_CleanCode_Refactoring/Bai_Tap/TennisGame.java
+++ project/src/ss8_CleanCode_Refactoring/Bai_Tap/TennisGame.java
@@ -1,47 +1,43 @@
 package ss8_CleanCode_Refactoring.Bai_Tap;
 
+import java.util.Scanner;
+
 public class TennisGame {
-    public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
+    public static String getScore(String player1Name, String player2Name, int score1, int score2) {
+        String score = " ";
+        int tempScore = 0;
+        if (score1==score2) {
+            switch (score1) {
                 case 0:
-                    score = "Love-All";
+                    score = "Love";
                     break;
                 case 1:
-                    score = "Fifteen-All";
+                    score = "Fifteen";
                     break;
                 case 2:
-                    score = "Thirty-All";
+                    score = "Thirty";
                     break;
                 case 3:
-                    score = "Forty-All";
+                    score = "Forty";
                     break;
                 default:
                     score = "Deuce";
                     break;
-
             }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
+        } else if (score1>=4 || score2>=4) {
+            int minusResult = score1-score2;
             if (minusResult==1) score ="Advantage player1";
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
             else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
+        } else {
+            for (int i=1; i<3; i++) {
+                if (i==1) {
+                    tempScore = score1;
+                }else{
+                    score+="-"; tempScore = score2;
+                }
+                switch(tempScore){
                     case 0:
                         score+="Love";
                         break;
