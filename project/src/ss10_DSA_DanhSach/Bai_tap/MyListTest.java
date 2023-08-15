276094c11d15e4394e86e98fd6d013a62162b996
HoNgocAn <anhongoc1306@gmail.com>
2023-08-15 10:58:01 +0700

    nop bai

---
+++ project/src/ss10_DSA_DanhSach/Bai_tap/MyListTest.java
@@ -0,0 +1,16 @@
+package ss10_DSA_DanhSach.Bai_tap;
+
+public class MyListTest {
+    MyList myList = new MyList(10);
+    public static void main(String[] args) {
+        MyList myList = new MyList(10);
+        System.out.println(myList.size());
+        myList.add(0, "Ho");
+        myList.add(1, "Ngoc ");
+        myList.add(2, "An");
+
+        System.out.println(myList);
+        myList.remove(4);
+        System.out.println(myList);
+    }
+}
--- project/src/ss8_CleanCode_Refactoring/Bai_Tap/MVC/Main/Delete.java
+++
@@ -1,7 +0,0 @@
-package ss8_CleanCode_Refactoring.Bai_Tap.MVC.Main;
-
-public class Delete {
-    public static void main() {
-        System.out.println("Delete information");
-    }
-}
