be2eeafb1d6f6fc4ef756f61c1958b58a276e4d1
HoNgocAn <anhongoc1306@gmail.com>
2023-08-17 20:39:39 +0700

    nop bai

--- module2/module2/src/ss13_thuattoantimkiem/bai_tap/Exersise1.java
+++ module2/module2/src/ss13_thuattoantimkiem/bai_tap/Exersise1.java
@@ -1,4 +1,43 @@
 package ss13_thuattoantimkiem.bai_tap;
 
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Exersise1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Nhập chuỗi: ");
+        String string = input.nextLine();
+
+        LinkedList<Character> max = new LinkedList<>();
+        for (int i = 0; i < string.length(); i++) {
+            LinkedList<Character> list = new LinkedList<>();
+            list.add(string.charAt(i));
+            for (int j = i + 1; j < string.length(); j++) {
+                if (string.charAt(j) > list.getLast()) {
+                    list.add(string.charAt(j));
+                }
+            }
+            if (list.size() > max.size()) {
+                max.clear();
+                max.addAll(list);
+            }
+            list.clear();
+        }
+        for (Character ch : max) {
+            System.out.print(ch);
+        }
+        System.out.println();
+    }
 }
+//                            Độ phức tạp của bài toán:
+//                    - 1 vòng lặp bên ngoài;
+//                    - 1 vòng lặp bên trong;
+//                    - 1 câu lệnh;
+//                    - 1 vòng lặp với 1 câu lệnh;
+
+
+
+
+
--- module2/module2/src/ss13_thuattoantimkiem/bai_tap/Exersise2.java
+++ module2/module2/src/ss13_thuattoantimkiem/bai_tap/Exersise2.java
@@ -1,5 +1,6 @@
 package ss13_thuattoantimkiem.bai_tap;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exersise2 {
@@ -7,33 +8,31 @@ public class Exersise2 {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter size");
         int size = input.nextInt();
-        int [] arr = new int[size];
+        int[] arr = new int[size];
         for (int i = 0; i < size; i++) {
             System.out.print("Enter element ");
             arr[i] = input.nextInt();
         }
-        int temp;
-        for (int i = 0; i < size; i++) {
-            for (int j = i + 1; j < size; j++) {
-                if (arr[i] > arr[j]) {
-                    temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-        }
+        Arrays.sort(arr);
+
+        
         System.out.println("Array whose elements are ");
         for (int i = 0; i < size; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
         System.out.print("Enter the element you want to search for ");
-        int n;
-        System.out.println("The element appears in the array and is at position " + binarySearch(arr, n = input.nextInt()));
+        int n = input.nextInt();
+        if (binarySearch(arr, n , 0, arr.length - 1) == -1) {
+            System.out.println("The element does not appear in the array");
+        } else {
+            System.out.println("The element appears in the array and is at position " + binarySearch(arr, n, 0, arr.length - 1));
+        }
     }
-    static int binarySearch(int[] arr,int value){
-        int left = 0;
-        int right = arr.length -1;
+
+    static int binarySearch(int[] arr,int value,int left,int right){
+        left = 0;
+        right = arr.length -1;
         while(right>=left){
             int mid = (left+right)/2;
             if (value<arr[mid]){
