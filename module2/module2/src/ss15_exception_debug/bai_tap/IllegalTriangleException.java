23db8b7c6924bf346e22097e9c8610ee3fcccf6d
HoNgocAn <anhongoc1306@gmail.com>
2023-08-21 20:32:54 +0700

    nop bai

--- module2/module2/src/ss15_exception_debug/bai_tap/IllegalTriangleException.java
+++ module2/module2/src/ss15_exception_debug/bai_tap/IllegalTriangleException.java
@@ -5,22 +5,26 @@ import java.util.Scanner;
 public class IllegalTriangleException {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        try {
-            System.out.println("Nhập cạnh a");
-            double a = input.nextDouble();
-            System.out.println("Nhập cạnh b");
-            double b = input.nextDouble();
-            System.out.println("Nhập cạnh c");
-            double c = input.nextDouble();
-            if  (a<=0 || b<=0 || c<= 0){
-                System.out.println("Không đúng");
-            } else if ((a+b>c) && (b+c)>a && (c+a)>b) {
-                System.out.println("Đúng");
-            } else {
-                System.out.println("Không đúng");
-            }
-        }catch (Exception e){
-            System.out.println("Đề nghị nhập lại các cạnh của tam giác");
-        }
+        double a = 0;
+        double b = 0;
+        double c = 0;
+        do {
+            try {
+                System.out.println("Nhập cạnh a");
+                a = input.nextDouble();
+                System.out.println("Nhập cạnh b");
+                b = input.nextDouble();
+                System.out.println("Nhập cạnh c");
+                c = input.nextDouble();
+                if (a <= 0 || b <= 0 || c <= 0 || (a+b)<=c || (a+c)<=b || (b+c)<=a){
+                    System.out.println("Đây không phải là 3 cạnh của một tam giác. Đề nghị nhập lại");
+                } else {
+                System.out.println("Đây là 3 cạnh của một tam giác");
+                }
+                } catch (Exception e) {
+                    System.out.println("Đề nghị nhập đúng định dạng");
+                    break;
+                }
+        }while (a <= 0 || b <= 0 || c <= 0 || (a+b)<=c || (a+c)<=b || (b+c)<=a ) ;
     }
 }
