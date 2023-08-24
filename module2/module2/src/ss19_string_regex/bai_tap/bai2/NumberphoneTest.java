13ff815fea4da4e2c517789d022388b52adce82d
HoNgocAn <anhongoc1306@gmail.com>
2023-08-24 19:55:41 +0700

    nop bai

--- module2/module2/src/ss19_string_regex/bai_tap/bai2/NumberphoneTest.java
+++ module2/module2/src/ss19_string_regex/bai_tap/bai2/NumberphoneTest.java
@@ -2,8 +2,8 @@ package ss19_string_regex.bai_tap.bai2;
 
 public class NumberphoneTest {
     private static Numbersphone numbersphone;
-    public static final String [] validNumbersphone = {"84-0978489648"};
-    public static final String [] invalidNumbersphone ={"a8-22222222", "84-22b22222","84-9978489648"};
+    public static final String [] validNumbersphone = {"(84)-(0978489648)"};
+    public static final String [] invalidNumbersphone ={"(a8)-(22222222)", "(84)-(22b22222)","(84)-(9978489648)"};
 
     public static void main(String[] args) {
         numbersphone = new Numbersphone();
